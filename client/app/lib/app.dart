bd8f71f8b17e0e97ac5f5cf8040784e10f1e68da
Peter Swanson <topazoo42@gmail.com>
2023-10-20 23:50:20 -0700

    [Client] Set name from env

--- client/app/lib/app.dart
+++ client/app/lib/app.dart
@@ -1,16 +1,17 @@
 import 'package:app/pages/http_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'styles/theme.dart';
 
 class App extends StatelessWidget {
+  static final env = dotenv.env;
+
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // TODO - Configuration Manager from env
-      title: 'App Name',
+      title: env['APP_NAME'] ?? 'App Name',
       theme: ThemeData(
         primarySwatch: AppTheme.primarySwatch,
       ),
