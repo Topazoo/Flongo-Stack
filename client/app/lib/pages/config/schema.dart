77eefac6790fb38a9ad5c62bf1241253905a5aba
Peter Swanson <topazoo42@gmail.com>
2023-10-25 00:18:34 -0700

    [Schemas] Cleanup

--- client/app/lib/pages/config/schema.dart
+++ client/app/lib/pages/config/schema.dart
@@ -1,11 +1,9 @@
 import 'package:app/schemas/json_to_widget_schema.dart';
-import 'package:app/utilities/http_client.dart';
 import 'package:flutter/material.dart';
 
 class Config_JSON_To_Widget_Schema extends JSON_To_Widget_Schema {
   const Config_JSON_To_Widget_Schema();
 
-  
   @override
   Widget widgetFromJSON(BuildContext context, dynamic json, String apiURL, Function? deleteItem, Function? updateItem) {
     if (json is! List) {
@@ -28,11 +26,11 @@ class Config_JSON_To_Widget_Schema extends JSON_To_Widget_Schema {
               mainAxisSize: MainAxisSize.min,
               children: [
                 IconButton(
-                  icon: Icon(Icons.edit),
+                  icon: const Icon(Icons.edit),
                   onPressed: () => updateItem!(apiURL, context, item, index),
                 ),
                 IconButton(
-                  icon: Icon(Icons.delete),
+                  icon: const Icon(Icons.delete),
                   onPressed: () => deleteItem!(apiURL, context, item, index),
                 ),
               ],
@@ -40,43 +38,6 @@ class Config_JSON_To_Widget_Schema extends JSON_To_Widget_Schema {
           );
         },
       ),
-    );
-  }
-
-  Future<void> _editConfig(int index, BuildContext context, Map<String, dynamic> config, String apiURL, Function? updateItem) async {
-    // Here you can show a dialog or navigate to another screen to get input from the user
-    // For this example, I'm simply updating the value to `newValue` for the sake of simplicity
-    final updatedValue = 'newValue';
-
-    dynamic newVal = {
-      '_id': config['_id'],
-      'value': updatedValue,
-    };
-
-    await HTTPClient(apiURL).patch(
-      body: newVal,
-      onSuccess: (response) {
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Updated Successfully')));
-        updateItem!(index, newVal);
-      },
-      onError: (error) {
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Failed to Update')));
-      },
-    );
-  }
-
-  Future<void> _deleteConfig(int index, BuildContext context, Map<String, dynamic> config, String apiURL, Function? deleteItem) async {
-    await HTTPClient(apiURL).delete(
-      queryParams: {
-        '_id': config['_id'].toString(),
-      },
-      onSuccess: (response) {
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Deleted Successfully')));
-        deleteItem!(index);
-      },
-      onError: (response) {
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Failed to Delete')));
-      },
     );
   }
 }
