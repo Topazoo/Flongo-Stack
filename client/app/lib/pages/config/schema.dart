d21c12db5c77878c58d87043023c88041c90761a
Peter Swanson <topazoo42@gmail.com>
2023-10-25 00:15:40 -0700

    [Schemas] Bones of easy page schema management

--- client/app/lib/pages/config/schema.dart
+++ client/app/lib/pages/config/schema.dart
@@ -1,26 +1,81 @@
 import 'package:app/schemas/json_to_widget_schema.dart';
+import 'package:app/utilities/http_client.dart';
 import 'package:flutter/material.dart';
 
 class Config_JSON_To_Widget_Schema extends JSON_To_Widget_Schema {
   const Config_JSON_To_Widget_Schema();
 
+  
   @override
-  Widget widgetFromJSON(BuildContext context, dynamic json) {
+  Widget widgetFromJSON(BuildContext context, dynamic json, String apiURL, Function? deleteItem, Function? updateItem) {
     if (json is! List) {
       throw ArgumentError('Expected a List but got ${json.runtimeType}');
     }
 
     List<Map<String, dynamic>> dataList = List<Map<String, dynamic>>.from(json);
 
-    return ListView.builder(
-      itemCount: dataList.length,
-      itemBuilder: (BuildContext context, int index) {
-        var item = dataList[index];
-        return ListTile(
-          leading: const Icon(Icons.info),
-          title: Text(item['name'] ?? ''),
-          subtitle: Text('${item['value']}'),
-        );
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: ListView.builder(
+        itemCount: dataList.length,
+        itemBuilder: (BuildContext context, int index) {
+          var item = dataList[index];
+          return ListTile(
+            leading: const Icon(Icons.info),
+            title: Text(item['name'] ?? ''),
+            subtitle: Text('${item['value']}'),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () => updateItem!(apiURL, context, item, index),
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () => deleteItem!(apiURL, context, item, index),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Future<void> _editConfig(int index, BuildContext context, Map<String, dynamic> config, String apiURL, Function? updateItem) async {
+    // Here you can show a dialog or navigate to another screen to get input from the user
+    // For this example, I'm simply updating the value to `newValue` for the sake of simplicity
+    final updatedValue = 'newValue';
+
+    dynamic newVal = {
+      '_id': config['_id'],
+      'value': updatedValue,
+    };
+
+    await HTTPClient(apiURL).patch(
+      body: newVal,
+      onSuccess: (response) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Updated Successfully')));
+        updateItem!(index, newVal);
+      },
+      onError: (error) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Failed to Update')));
+      },
+    );
+  }
+
+  Future<void> _deleteConfig(int index, BuildContext context, Map<String, dynamic> config, String apiURL, Function? deleteItem) async {
+    await HTTPClient(apiURL).delete(
+      queryParams: {
+        '_id': config['_id'].toString(),
+      },
+      onSuccess: (response) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Deleted Successfully')));
+        deleteItem!(index);
+      },
+      onError: (response) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Failed to Delete')));
       },
     );
   }
--- client/app/lib/pages/json_page.dart
+++ client/app/lib/pages/json_page.dart
@@ -1,5 +1,6 @@
 import 'package:app/pages/http_page.dart';
 import 'package:app/schemas/json_to_widget_schema.dart';
+import 'package:app/utilities/http_client.dart';
 import 'package:flutter/material.dart';
 
 class JSON_Page extends API_Page {
@@ -12,6 +13,76 @@ class JSON_Page extends API_Page {
 }
 
 class JSON_PageState extends API_PageState<JSON_Page> {
+  void _handleDelete(int? index) {
+    if (index != null) {
+      if (data is! List) {
+        throw IndexError.withLength(index, 0, message: "Can't remove a non list item by index");
+      }
+      setState(() {
+        data.removeAt(index);
+      });
+    } else {
+      if (data is List) {
+        throw IndexError.withLength(0, data.length, message: "Must pass index of item to remove!");
+      }
+      setState(() {
+        data = null;
+      });
+    }
+  }
+
+  Future<void> deleteItem(String apiURL, BuildContext context, Map<String, dynamic> item, int? index) async {
+    await HTTPClient(apiURL).delete(
+      queryParams: {
+        '_id': item['_id'].toString(),
+      },
+      onSuccess: (response) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Deleted Successfully')));
+        _handleDelete(index);
+      },
+      onError: (response) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Failed to Delete')));
+      },
+    );
+  }
+
+  void _handleUpdate(int? index, dynamic newData) {
+    if (index != null) {
+      if (data is! List) {
+        throw IndexError.withLength(index, 0, message: "Can't update a non list item by index");
+      }
+      setState(() {
+        data[index] = newData;
+      });
+    } else {
+      if (data is List) {
+        throw IndexError.withLength(0, data.length, message: "Must pass index of item to remove!");
+      }
+      setState(() {
+        data = newData;
+      });
+    }
+  }
+  
+
+  Future<void> updateItem(String apiURL, BuildContext context, Map<String, dynamic> item, int? index) async {
+    final Map<String, dynamic> newVal = Map.from(item);
+    newVal['value'] = "UPDATED"; // Get this value from a modal using `item` along with any other fields
+
+    await HTTPClient(apiURL).patch(
+      body: newVal,
+      onSuccess: (response) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Updated Successfully')));
+        _handleUpdate(index, newVal);
+      },
+      onError: (error) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Failed to Update')));
+      },
+    );
+  }
+
   @override
-  Widget getPageWidget(BuildContext context) => widget.schema.widgetFromJSON(context, data);
+  Widget getPageWidget(BuildContext context) => 
+    widget.schema.widgetFromJSON(context, data, widget.apiURL, deleteItem, updateItem);
 }
+
