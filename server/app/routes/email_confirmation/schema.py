55daf0142d4707a56a024349b528429a1b5e300a
Peter Swanson <topazoo42@gmail.com>
2023-11-12 14:38:53 -0800

    [Email] Fix verification

--- server/app/routes/email_confirmation/schema.py
+++ server/app/routes/email_confirmation/schema.py
@@ -26,5 +26,19 @@ EMAIL_CONFIRMATION_ROUTE_REQUEST_SCHEMA = Route_Schema(
             }
         },
         'required': ['username', 'email_address']
+    },
+
+    # Resend a confirmation email
+    PUT = {
+        'type': 'object',
+        'additionalProperties': False,
+        'properties': {
+            'username': {'type': 'string', "minLength": 4},
+            'email_address': {
+                'type': 'string',
+                'pattern': r'^([a-zA-Z0-9_\-\.\+]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$'
+            }
+        },
+        'required': ['username', 'email_address']
     }
 )
